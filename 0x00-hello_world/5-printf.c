0d818867fd2d1ec63affed992acf054473201c85
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-04-09 04:46:41 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase with proper grammar
+ * but the outcome is a piece of art
+ * Return:0 (Success) when exited successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
