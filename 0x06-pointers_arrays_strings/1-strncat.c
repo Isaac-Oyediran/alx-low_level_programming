7b9ad0b724d5d0f2a334602c92fe09b85ded0298
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-04-24 15:11:07 -0700

    task 3

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * *_strncat - like _strcat but it will use n bytes fron scr
+ * *_strncat - like _strcat but it will use n bytes from src
  * @dest: string to be concatenated
- * @scr: string to be concatenated until n limit
- * @n: number of bytes to be used from src
+ * @src: string to be concatenated until n limit
+ * @n: number of bytes that will be used from src
  * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
@@ -12,9 +12,4 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (x = 0; dest[x] != '\0'; x++)
 		;
-	for (y = 0; src[y] != '\0' && n > 0; y++, n--, x++)
-	{
-		dest[x] = src[y];
-	}
-	return (dest);
-}
+	for (y = 0; src[y] !
