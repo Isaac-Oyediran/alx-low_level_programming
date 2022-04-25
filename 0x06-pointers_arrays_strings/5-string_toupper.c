d476eeaf5ba77edabc18e6ba1540bb55ae6f0736
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-04-25 12:00:46 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,19 @@
-#include 
+#include "main.h"
+/**
+ * string_toupper - convert string to uppercase
+ * @s: string to be converted
+ * Return: s
+ */
+char *string_toupeer(char *)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] > 96 && s[x] < 123)
+		{
+			s[x] -= 32
+		}
+	}
+	return (s);
+}
