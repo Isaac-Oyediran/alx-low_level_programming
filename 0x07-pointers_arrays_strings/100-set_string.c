c6a676b486a4ac711d692db6319af1e922b541ce
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-04-26 15:21:52 -0700

    task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,11 +2,11 @@
 
 /**
  * set_string - a function that sets the value of a pointer to a char
- * @s: double pointer to string
+ * @s: double pointer to a string
  * @to: pointer to the string to set s to
  * Return: Description of the returned value
  */
 void set_string(char **s, char *to)
 {
-	*s = to
+	*s = to;
 }
