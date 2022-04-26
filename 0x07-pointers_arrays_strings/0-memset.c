145600eb251eb1a2e7233fac365e52a298f714d1
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-04-26 15:49:08 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	i = 0;
+	unsigned int i; i = 0;
 
 	while (i < n)
 	{
-		s[i] = b;
-		i++;
+		s[i] = b; i++;
 	}
 	return (s);
 }
