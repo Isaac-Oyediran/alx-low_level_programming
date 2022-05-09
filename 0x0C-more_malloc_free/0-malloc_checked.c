af9a7f9aeee556088090fafa5083ca44e2d61fb5
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-05-09 14:57:09 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,19 @@
 #include "main.h"
-
 /**
  * malloc_checked - allocates memory using malloc
  * @b: size of memory to be allocated
- * Return: Pointer
+ * Return: void
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *p;
 
 	p = malloc(b);
 	if (p == NULL)
-		exit (98);
+	{
+		exit(98);
+	}
+
 	return (p);
 }
