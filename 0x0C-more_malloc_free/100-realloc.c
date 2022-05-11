6cf64e4483c8f22f124df450a71fe290a599db3f
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-05-11 05:33:02 -0700

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,7 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int n;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
@@ -50,7 +50,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	ptr2 = malloc(new_size);
-	if ptr2 == 0
+	if (ptr2 == 0)
 		return (0);
 
 	_memcpy(ptr2, ptr, old_size);
