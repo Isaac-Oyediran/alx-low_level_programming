28bc264e235c6cf70289439b1786c84e4a538a8c
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-05-10 19:07:26 -0700

    main.h

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _memset - initializes n bytes of memory to x
+ * @ptr: initial address
+ * @x: variable to initialize with
+ * @n : number of bytes to initialize
+ *
+ * Return: Return pointer char (so movements are 1 byte)
+ */
+
