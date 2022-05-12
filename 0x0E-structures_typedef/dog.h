f4fe65b1f4e82800edb4e2540830392a34359985
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-05-11 18:00:58 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,20 +5,23 @@
  * @name: The name of the dog
  * @age: The age of the dog
  * @owner: The owner of the dog
- * Description: Longer description
  */
-typedef struct dog
+struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
-} dog_t;
-#endif
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 int _putchar(char c);
-void init_dog(struct dog *d, char *name, float *age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float *age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
