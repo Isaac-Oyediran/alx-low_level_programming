d0937752c41a1130251addf6576bb7e6319876dd
Isaac Oyediran <isaacoyediran57@gmail.com>
2022-05-20 01:58:47 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,9 +4,9 @@
  * main - main block
  * Description: Prints all possible combinations for single-digit numbers
  * Numbers must be separated by commas and a space
- * You can only use putchar to print to console
- * You can only use putchar up to four times
- * You are not allowed to use any variable of type char
+ * You can only use `putchar` to print to console
+ * You can only use `putchar` up to four times
+ * You are not allowed to use any variable of type `char`
  * Return: 0
  */
 int main(void)
